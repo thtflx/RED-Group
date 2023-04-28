9cac8b40019944b8b370d2b539b8f0713c1de4f9
Aziz Sattorov <108568027+thtflx@users.noreply.github.com>
2023-04-28 19:57:52 +0300

    add changes

---
+++ vite-project-v1/src/components/AlwaysMode.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function AlwaysMode() {
+    return (
+        <div>
+            <nav>
+                <h1>AlwaysMode activated!</h1>
+            </nav>
+        </div>
+    )
+}
