7dcf48f3b8352d04ab42ee99fb9b894c2f6c42db
Aziz Sattorov <108568027+thtflx@users.noreply.github.com>
2023-04-30 17:19:15 +0300

    add data

---
+++ vite-project-v2/src/cars.data.js
@@ -0,0 +1,68 @@
+export const cars = [
+    {
+        id: 1,
+        name: 'Toyota Camry',
+        price: 24943,
+        image: 'https://i.cenyavto.com/2021/03/Vdhx-scaled.jpeg'
+    },
+    {
+        id: 2,
+        name: 'Honda Civic',
+        price: 22190,
+        image: 'https://motor.ru/imgs/2021/11/16/07/5029149/c6a078b3c3448720cda902894d0841eb23848849.jpg'
+    },
+    {
+        id: 3,
+        name: 'Nissan Altima',
+        price: 24990,
+        image: 'https://s.auto.drom.ru/i24235/c/photos/fullsize/nissan/altima/nissan_altima_904699.jpg'
+    },
+    {
+        id: 4,
+        name: 'Ford Mustang',
+        price: 28365,
+        image: 'https://dvizhok.su/i/files2/auto/2022/09/Ford_Mustang_gen7_2.jpg'
+    },
+    {
+        id: 5,
+        name: 'Chevrolet Corvette',
+        price: 67650,
+        image: 'https://autoreview.ru/images/Article/1729/Article_172925_860_575.jpg'
+    },
+    {
+        id: 6,
+        name: 'BMW 3 Series',
+        price: 41350,
+        image: 'https://www.allcarz.ru/wp-content/uploads/2022/05/foto-3-series-2023_01.jpg'
+    },
+    {
+        id: 7,
+        name: 'Mercedes-Benz C-Class',
+        price: 41700,
+        image: 'https://st2.zr.ru/_ah/img/udlvmQUG22epNCKJF4lhZA=s800'
+    },
+    {
+        id: 8,
+        name: 'Audi A4',
+        price: 40200,
+        image: 'https://img-c.drive.ru/models.photos/0000/000/000/001/9ad/48d8a14622c7444e-large.jpg'
+    },
+    {
+        id: 9,
+        name: 'Tesla Model 3',
+        price: 39990,
+        image: 'https://rg.ru/uploads/images/229/30/04/12.jpg'
+    },
+    {
+        id: 10,
+        name: 'Porsche 911',
+        price: 101200,
+        image: 'https://autoreview.ru/images/Article/1748/Article_174830_860_575.jpg'
+    },
+    {
+        id: 11,
+        name: 'Ferrari 488 GTB',
+        price: 262647,
+        image: 'https://ferrari-avilon.ru/media/blocks/info_sections/showroom-min.jpeg'
+    }
+]
