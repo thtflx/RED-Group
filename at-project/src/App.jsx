d78e3883321d3bd4e1ddfc4ed77fcf775020c718
Aziz Sattorov <108568027+thtflx@users.noreply.github.com>
2023-05-02 15:41:42 +0300

    initial commit

---
+++ at-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import AlwaysMode from './components/AlwaysMode'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <AlwaysMode />
+      </div>
+
+    </div>
+  )
+}
+
+export default App
