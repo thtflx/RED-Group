43b9cb12617a7f3d66be310bb51ff8fd600e3597
Aziz Sattorov <108568027+thtflx@users.noreply.github.com>
2023-05-05 21:17:16 +0300

    upd

--- at-project/src/App.jsx
+++ at-project/src/App.jsx
@@ -1,29 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import List from './List/List';
+import MainLayout from './Layouts/MainLayout';
 
 export default function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const getData = async() => {
-      const request = await fetch("https://restcountries.com/v3.1/all");
-      const response = await request.json();
-
-      setData(response);
-    }
-    getData();
-  }, [])
-
   
-
   return (
-    <>
-      <h1 className='text-[40px] flex justify-center ml-auto mr-auto bg-slate-400'>HEADER</h1>
-      <main>
-        <List data={data}/>
-      </main>
-      <h1 className='text-[40px] flex justify-center ml-auto mr-auto bg-slate-400'>FOOTER</h1>
-
-    </>
+    <div>
+      <MainLayout/>
+    </div>
   )
 }
