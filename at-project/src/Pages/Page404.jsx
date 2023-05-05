21ca49cee4984408d366e3cb360d04dcf950b486
Aziz Sattorov <108568027+thtflx@users.noreply.github.com>
2023-05-05 21:17:31 +0300

    upd

---
+++ at-project/src/Pages/Page404.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Page404() {
+    return (
+        <div>
+            <h1 className='flex justify-center text-center text-[30px] bg-red-300 px-2 py-2'>404 <br/>Not Found</h1>
+        </div>
+    )
+}
