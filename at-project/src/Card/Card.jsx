0772f53c200b5eaa9ed3ebda7bfddfe5f0ed7003
Aziz Sattorov <108568027+thtflx@users.noreply.github.com>
2023-05-05 21:17:40 +0300

    upd

--- at-project/src/Card/Card.jsx
+++ at-project/src/Card/Card.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Card = (props) => {
     return (
-        <div>
-            <div className='card px-2 py-2 border-black border-[2px]'>
-                <img className='flex justify-center ml-auto mr-auto' src={props.flags.png} />
-                <h3 className='flex justify-center ml-auto mr-auto'>{props.name.common}</h3>
-                <p className='flex justify-center ml-auto mr-auto'>{props.subregion}</p>
+        <Link to={`/country/${props.name.common}`}>
+            <div>
+                <div className='card px-2 py-2 border-black border-[2px]'>
+                    <img className='flex justify-center ml-auto mr-auto' src={props.flags.png} />
+                    <h3 className='flex justify-center ml-auto mr-auto'>{props.name.common}</h3>
+                    <p className='flex justify-center ml-auto mr-auto'>{props.subregion}</p>
+                </div>
             </div>
-        </div>
+        </Link>
 )
 }
 
